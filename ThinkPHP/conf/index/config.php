be2236a784c24ebfd772fbbbc01693bdc9481fd6
Moriarty123 <1561357687@qq.com>
2019-11-08 03:23:09 +0800

    添加util option plugin 文件夹

---
+++ ThinkPHP/conf/index/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // 视图输出字符串内容替换
+    'view_replace_str'       => [
+        '__CSS__'           => '/static/css',
+        '__JS__'            => '/static/js',
+        '__IMG__'           => '/static/images',
+    ],
+];
